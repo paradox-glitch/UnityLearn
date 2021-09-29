1fdc28dafce508e1f76c705e9d0e12372274e34d
Morgan Finney <dev@paradox-glitch.co.uk>
2021-09-29 01:09:07 +0100

    Roll-a-Ball | Moving the Camera
    Learnt: Nothing

---
+++ Roll-a-Ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject _player;
+
+    private Vector3 _offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _offset = transform.position - _player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = _player.transform.position + _offset;
+    }
+}
